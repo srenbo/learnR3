2a75f5185b185ef4b3cba148f175acd6516c1a00
Søren Bønløkke <srenbon@gmail.com>
2023-04-25 15:44:04 +0200

    Added R foder

---
+++ R/learning.R
@@ -0,0 +1,15 @@
+# Basics of R --------------------------------------
+
+colnames(airquality)
+
+str(airquality)
+
+testThisFush <- "bla"
+mean(2:6)
+
+
+
+# Loading packages --------------------------------------------------------
+
+library(tidyverse)
+library(Hmisc)
