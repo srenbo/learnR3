321388ffa55c5a441dda66e9b053f8419bded110
Søren Bønløkke <srenbon@gmail.com>
2023-04-26 11:53:03 +0200

    pipe stuff

--- R/learning.R
+++ R/learning.R
@@ -1,21 +1,70 @@
-# Basics of R --------------------------------------
-
-colnames(airquality)
-
-str(airquality)
-
-testThisFush <- "bla"
-mean(2:6)
-
-
-
-# Loading packages --------------------------------------------------------
-
+# Load packages
 library(tidyverse)
-library(Hmisc)
+library(NHANES)
 
-# this will be used for testing git
+# Looking at data
+glimpse(NHANES)
 
-#test this sh*****
 
-#why does this need to be so complicated
+
+# Selecting columns -------------------------------------------------------
+
+select(NHANES, Age)
+
+select(NHANES, Age, Weight, BMI)
+
+select(NHANES, -HeadCirc)
+
+select(NHANES, starts_with("BP"))
+
+select(NHANES, ends_with("Day"))
+
+select(NHANES, contains("Age"))
+
+# Creating smaller NHANES dataset
+
+nhanes_small <- select(
+  NHANES, Age, Gender, BMI, Diabetes, PhysActive,
+  BPSysAve, BPDiaAve, Education
+)
+
+
+
+# Renaming columns --------------------------------------------------------
+
+nhanes_small <- rename_with(
+  nhanes_small,
+  snakecase::to_snake_case
+)
+
+
+# Renaming specific columns -----------------------------------------------
+
+nhanes_small <- rename(nhanes_small, sex = gender)
+
+nhanes_small
+
+
+# Trying out the pipe -----------------------------------------------------
+
+colnames(nhanes_small)
+
+nhanes_small %>%
+  colnames()
+
+nhanes_small %>%
+  select(phys_active) %>%
+  rename(physically_active = phys_active)
+
+
+# Exercise 7.8 ------------------------------------------------------------
+
+nhanes_small %>% select(bp_sys_ave, education)
+
+nhanes_small %>% rename(bp_sys = bp_sys_ave, bp_dia = bp_dia_ave)
+
+nhanes_small %>% select(bmi, contains("age"))
+
+nhanes_small %>%
+  select(starts_with("bp_")) %>%
+  rename(bp_systolic = bp_sys_ave)
